9f01558b8139ebcdafb20376cfdc9114a585b4a1
sychan2 <31824583+sychan2@users.noreply.github.com>
2017-10-01 12:30:30 -0400

    fixed procedures

--- login.php
+++ login.php
@@ -1,9 +1,12 @@
 <?php
-	// Adding functions to validate login and connect to the server
-	require_once("connection.php");
 
+// Adding functions to validate login and connect to the server
+require_once("connection.php");
+
+function login() {
 	$user = $_GET["user"];
 	$pass = $_GET["pass"];
 
 	$results = databaseConnection($user, $pass);
+}
 ?>
