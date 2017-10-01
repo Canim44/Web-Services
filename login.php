bc9f9b17a5d5c0f30e7ea3f20a3dec4076c0bfed
sychan2 <31824583+sychan2@users.noreply.github.com>
2017-10-01 10:08:20 -0400

    st

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php
-function validateLogin($results) {
-	$authenticated = "[{\"COUNT(*)\":\"1\"}]";
-	
-	if ($results == $authenticated) {
-		return 1;
-	}	
-	else {
-		return 0;
+	function validateLogin($results) {
+		$authenticated = "[{\"COUNT(*)\":\"1\"}]";
+
+		if ($results == $authenticated) {
+			return 1;
+		}	
+		else {
+			return 0;
+		}
 	}
-}
 ?>
