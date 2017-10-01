8c57c58f73966d495b06f11535b62994eb674f6c
sychan2 <31824583+sychan2@users.noreply.github.com>
2017-10-01 15:17:14 -0400

    test

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 // Adding functions to validate login and connect to the server
 include_once("connection.php");
-
+echo "here";
 function login() {
 	$user = $_GET["user"];
 	$pass = $_GET["pass"];
