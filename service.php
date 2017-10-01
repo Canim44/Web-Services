ebd7198bad24ca5dbcf39fbadec0d3b159a896d6
UK Fall 2017 G1 <sychan2@uky.edu>
2017-09-30 20:27:48 -0400

    stuff

--- service.php
+++ service.php
@@ -1,19 +1,20 @@
 <?php
 // Include the header file for JSON services and library
-include_once("JSON.php");
+
+include("JSON.php");
 // Initiating the JSON service
 //$json = new Services_JSON();
 
 // Initializing the link to the MySQL database
 $link = mysqli_connect("localhost", "ukfl2017g2", "Ekwi32D98") or die("Could not connect");
-mysql_select_db("ukfl2017g2") or die("Could not select database");
+mysqli_select_db($link, "ukfl2017g2") or die("Could not select database");
+
 $arr = array();
 
 $rs = mysqli_query($link, "SELECT * FROM users");
 while($obj = mysqli_fetch_object($rs)) {
 	$arr[] = $obj;
 }
-echo "here"; 
 //Echo $json->encode($arr);
-*/
+
 ?>
