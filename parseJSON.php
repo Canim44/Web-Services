297ca1ab8c92ad9afd76c4424045a3d395f93abb
Steve <sychan2@g.uky.edu>
2017-10-26 18:39:17 -0400

    stae

--- parseJSON.php
+++ parseJSON.php
@@ -1,27 +1,77 @@
 <?php
-$symbol = "symbol";
-$name = "name";
-$price = 1.00;
-
 function parseStock($output)
 {
     $parsePosition = 0;
-    for ($i = 0; $i < 4; $i++) {
-      $parsePosition = skipQuotes($output, $parsePosition);
-    }
+    $endQuote = 0;
 
-    $endQuote = strpos($output, "\"", $parsePosition);
-    $symbol = substr($output, $parsePosition, $endQuote + $parsePosition);
+    $symbol = getField($output, $parsePosition, $endQutoe, "symbol");
+    $exchange = getField($output, $parsePosition, $endQutoe, "exchange");
+    $name = getField($output, $parsePosition, $endQutoe, "name");
+    $change = getField($output, $parsePosition, $endQuote, "c");
+    $price = getField($output, $parsePosition, $endQuote, "l");
+
+    // do {
+    //   $symbol = getField($output, $parsePosition, $endQuote);
+    //   $parsePosition = $endQuote + 1;
+    // } while ($symbol != "symbol");
+    // $symbol = getField($output, $parsePosition, $endQuote);
+    //
+    // do {
+    //   $exchange = getField($output, $parsePosition, $endQuote);
+    //   $parsePosition = $endQuote + 1;
+    // } while ($exchange != "exchange");
+    // $exchange = getField($output, $parsePosition, $endQuote);
+    //
+    // do {
+    //   $name = getField($output, $parsePosition, $endQuote);
+    //   $parsePosition = $endQuote + 1;
+    // } while ($name != "name");
+    // $name = getField($output, $parsePosition, $endQuote);
+    //
+    // do {
+    //   $change = getFiel($output, $parsePosition, $endQuote);
+    //   $parsePosition = $endQuote + 1;
+    // } while ($change != "c");
+    // $change = getField($output, $parsePosition, $endQuote);
+
     echo $symbol;
-
-    //$symbolPosition = strpos($output, "symbol\" : \"" );
-    //echo strpos($output, " : ", $symbolPosition);
-    //echo substr($output, 9, strpos($output," : ", $symbolPosition) - $symbolPosition - 1);
+    echo $exchange;
+    echo $name;
+    echo $change;
+    echo $price;
 }
 
-function skipQuotes($input, $index) {
-  $index = strpos($input, "\"");
-  return $index;
+function getField($input, &$startQuote, &$endQuote, $token) {
+  do {
+    if ($startQuote == 0) {
+      $startQuote = strpos($input, "\"");
+    }
+    else {
+      $startQuote = strpos($input, "\"", $startQuote);
+    }
+    $startQuote++;
+    $endQuote = strpos($input, "\"", $startQuote);
+    $stringReturn = substr($input, $startQuote, $endQuote - $startQuote);
+  } while ($stringReturn != $token);
+
+  $startQuote = $endQuote + 1;
+
+  $startQuote = strpos($input, "\"", $startQuote);
+  $startQuote++;
+  $endQuote = strpos($input, "\"", $startQuote);
+  $stringReturn = substr($input, $startQuote, $endQuote - $startQuote);
+
+  // if ($startQuote == 0) {
+  //   $startQuote = strpos($input, "\"");
+  // }
+  // else {
+  //   $startQuote = strpos($input, "\"", $startQuote);
+  // }
+  // $startQuote++;
+  // $endQuote = strpos($input, "\"", $startQuote);
+  // $stringReturn = substr($input, $startQuote, $endQuote - $startQuote);
+
+  return $stringReturn;
 }
 
 function copyToVariabel($input, $variable, $index, $endIndex) {
