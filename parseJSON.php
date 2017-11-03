e722b3fd9a18e9ec1b71e1b52afc35fd2bd1d45c
sychan2 <31824583+sychan2@users.noreply.github.com>
2017-11-03 15:04:44 -0400

    asdf

--- parseJSON.php
+++ parseJSON.php
@@ -45,7 +45,7 @@ echo $output;
     //         $i++;
     // } while ($i <= 2);
 }
-function getOptionData($input, $start, &$putsArray) {
+function getOptionData($input, $startPosition, &$putsArray) {
     // Pipe delimited string will be returned in the following form:
     // cid  |  price    |   change  | changePercent     |    fill   |   strike
     $startPosition = strpos($input, "\"cid\"", $startPosition);
