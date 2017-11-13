0c59ed0e0567d2f3b6ab9b3a23227f440b87c2ce
Steve <31824583+sychan2@users.noreply.github.com>
2017-11-13 17:25:39 -0500

    asdf

--- register.php
+++ register.php
@@ -9,6 +9,8 @@
 
   $link = serverConnection();
 
+ // escapeAt($email);
+
   // Query to check if anything is returned when trying to match by username
   $query = "SELECT COUNT(*) FROM users WHERE username = \"" . $username . "\"";
   // Run query
@@ -16,10 +18,17 @@
 
   $compareOutput = "[{\"COUNT(*)\":\"0\"}]";//$result;
   if (strcmp($result, $compareOutput) == 0) {
-    $registerQuery = "INSERT INTO users (username, password) VALUES ('". $username . "', '" . $password . ", '" . $email . "')";
+    $registerQuery = "INSERT INTO users (username, password, email) VALUES ('". $username . "', '" . $password . "', '" . $email . "')";
     $result = runNoReturnQuery($link, $registerQuery);
+    //echo $registerQuery;
   }
   else {
       echo "User exists";
+  }
+
+  function escapeAt(&$email) {
+      $position = strpos($email, "");
+
+      $email = substr($email, 0, $position) . "\\" . substr($email, $position);
   }
 ?>
