2704d67c953531f0b10176382330fe8418460651
Canim44 <connorcanim@gmail.com>
2017-11-16 18:20:27 -0500

    Removed static AAPL from url.

--- chartData.php
+++ chartData.php
@@ -4,7 +4,7 @@ include_once("connection.php");
 $symbol = $_GET['symbol'];
 
 // Grab file from Google Finance API, map to array, and encode to JSON
-$file = 'http://www.google.com/finance/historical?q=NASDAQ:' . symbol . 'AAPL&startdate=Jan+01%2C+2000&output=csv';
+$file = 'http://www.google.com/finance/historical?q=NASDAQ:' . symbol . '&startdate=Jan+01%2C+2000&output=csv';
 $csv = file_get_contents($file);
 $array = array_map("str_getcsv", explode("\n", $csv));
 $json = json_encode($array);
