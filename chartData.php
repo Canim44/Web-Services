777ebfe31d83c3b17690e8a1cd2c7ba4801d24b9
Canim44 <connorcanim@gmail.com>
2017-11-16 18:31:10 -0500

    Add error handling for bad ticker.

--- chartData.php
+++ chartData.php
@@ -4,8 +4,14 @@ include_once("connection.php");
 $symbol = $_GET['symbol'];
 
 // Grab file from Google Finance API, map to array, and encode to JSON
-$file = 'http://www.google.com/finance/historical?q=NASDAQ:' . $symbol . '&startdate=Jan+01%2C+2000&output=csv';
-$csv = file_get_contents($file);
+$file = 'http://www.google.com/finance/historical?q=NASDAQ:' 
+	. $symbol 
+	. '&startdate=Jan+01%2C+2000&output=csv';
+if (!file_exists($file)) {
+	die("Ticker not found");
+} else {
+	$csv = file_get_contents($file);
+}
 $array = array_map("str_getcsv", explode("\n", $csv));
 $json = json_encode($array);
 print_r($json);
